92794930294628c1cc53d01e3f84ade27e7af5c8
Christopher <chriswise@outlook.com>
2021-05-16 14:27:10 -0700

    adding in fonts, secret file example, and lib

---
+++ secrets.py
@@ -0,0 +1,9 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+secrets = {
+    'ssid' : 'wifi',
+    'password' : 'wifi password',
+    'timezone' : "America/Los_Angeles", # http://worldtimeapi.org/timezones
+    'cellarTrackerUsername' : 'cellartracker username',
+    'cellarTrackerPassword' : 'cellartracker password'
+    }
